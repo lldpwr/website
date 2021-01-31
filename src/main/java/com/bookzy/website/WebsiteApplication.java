5a6ca7a7c9a426ebe2666151d8ee6a2fb2a69823
Lloyd <lloyd@matrix.ca>
2021-01-30 19:27:40 -0500

    simple bookzy webpage with heading and title

--- src/main/java/com/bookzy/website/WebsiteApplication.java
+++ src/main/java/com/bookzy/website/WebsiteApplication.java
@@ -2,12 +2,21 @@ package com.bookzy.website;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@SpringBootApplication
+@SpringBootApplicatio
+@ResponseBody
 public class WebsiteApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebsiteApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String HomePage() {
+		return String.format("<!DOCTYPE html> <head> <title>Bookzy</title> </head> <body> <h1>Bookzy</h1> </body> </html>");
 	}
 
 }
