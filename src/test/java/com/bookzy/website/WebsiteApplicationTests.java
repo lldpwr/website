72f17a8da0509a87d5905b054e2d07a54d8c8414
Lloyd <lloyd@matrix.ca>
2021-01-30 19:28:37 -0500

    simple testing to find if bookzy is on the page

--- src/test/java/com/bookzy/website/WebsiteApplicationTests.java
+++ src/test/java/com/bookzy/website/WebsiteApplicationTests.java
@@ -2,12 +2,16 @@ package com.bookzy.website;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class WebsiteApplicationTests {
 
 	@Test
 	void contextLoads() {
+		WebsiteApplication test = new  WebsiteApplication();
+		String result = test.HomePage();
+		Assert.hasText(result,"Bookzy");
 	}
 
 }
