15d656f7cc0dcae4096f4f8ba242f7ad95a0107a
Lloyd <lloyd@matrix.ca>
2021-02-02 13:26:51 -0500

    mend
    Annotation of SpringBootApplication in WebsiteApplication missed a n, so there was compilation error and Annotation controller was missing so the root path genereate a error at runtime

--- src/main/java/com/bookzy/website/WebsiteApplication.java
+++ src/main/java/com/bookzy/website/WebsiteApplication.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
-@ResponseBody
+@Controller
 public class WebsiteApplication {
 
 	public static void main(String[] args) {
@@ -15,6 +15,7 @@ public class WebsiteApplication {
 	}
 
 	@GetMapping("/")
+	@ResponseBody
 	public String HomePage() {
 		return String.format("<!DOCTYPE html> <head> <title>Bookzy</title> </head> <body> <h1>Bookzy</h1> </body> </html>");
 	}
